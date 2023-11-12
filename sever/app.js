3066e869113589d7226e086554e9cc0a8a9b5ae1
lbrzr <akakpoleandre0gmail.com>
2023-11-12 10:53:21 +0100

    feat: db connection complete, user model register

--- sever/app.js
+++ sever/app.js
@@ -1,18 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const { connectDb } = require('./config/db');
+connectDb();
+
+// routers set up
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ sever/models/user.js
@@ -0,0 +1,29 @@
+const { model, Schema } = require('mongoose');
+
+const AutenticationSchema = new Schema({
+    password: {
+        type: String,
+    },
+    salt: {
+        type: String,
+    },
+    token: {
+        type: String,
+    },
+});
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    authentication: {
+        type: AutenticationSchema,
+    },
+});
+
+const UserModel = model('User', UserSchema);
+
+module.exports = { UserModel };
